666937e5426b65837c3273a5e9706e84a5194897
Maciej Krüger <mkg20001@gmail.com>
2021-02-06 15:16:50 +0100

    feat: copy some install stuff from postinst

--- package.nix
+++ package.nix
@@ -14,14 +14,14 @@
 
 stdenv.mkDerivation {
   pname = "safenet";
-  
+
   version = "9.0.43";
-  
+
   src = fetchurl {
     url = "https://download.comodo.com/SAC/linux/deb/x64/SafenetAuthenticationClient-9.0.43-0_amd64.deb";
     sha256 = "1an7wyhkwh9fkxg176g2y460wpkpxakyvy4lcv7rwip6pm341jk8";
   };
-  
+
   nativeBuildInputs = [
     dpkg
     autoPatchelfHook
@@ -36,7 +36,7 @@ stdenv.mkDerivation {
     pcsclite
     openssl.out
   ];
-  
+
   unpackPhase = ''
     dpkg -x $src source
     cd source
@@ -65,11 +65,48 @@ stdenv.mkDerivation {
     done
 
     mv usr/* $out
+    rm -rf etc/init.d
+    rm -rf etc/rc.d
     mv etc $out/etc
-    
+
     mkdir -p $out/pcsc/drivers
     cp -rf $out/share/eToken/drivers/aks-ifdh.bundle $out/pcsc/drivers
     chmod +x $out/pcsc/drivers/aks-ifdh.bundle/Contents/Linux/libAksIfdh.so.9.0
     ln -f -s libAksIfdh.so.9.0 $out/pcsc/drivers/aks-ifdh.bundle/Contents/Linux/libAksIfdh.so
+
+    # this mess is from postinst
+
+    clink ()
+    {
+      mkdir -p $(dirname $2)
+      ln -fs $1 $2
+    }
+
+    # kde
+    clink $out/share/eToken/shortcuts/SACTools.desktop $out/share/applications/SACTools.desktop
+    clink $out/share/eToken/shortcuts/SACMonitor.desktop $out/share/applications/SACMonitor.desktop
+    clink $out/share/eToken/shortcuts/SACMonitor.desktop $out/share/autostart/SACMonitor.desktop
+    clink $out/share/eToken/shortcuts/SafeNet.directory $out/share/desktop-directories/SafeNet.directory
+    clink $out/share/eToken/shortcuts/SafeNet-Authentication-Client.directory $out/share/desktop-directories/SafeNet-Authentication-Client.directory
+    clink $out/share/eToken/shortcuts/icons/SACTools.png $out/share/icons/SACTools.png
+    clink $out/share/eToken/shortcuts/icons/SACMonitor.png $out/share/icons/SACMonitor.png
+    touch $out/share/icons
+
+    # gnome
+    clink $out/share/eToken/shortcuts/SACTools.desktop $out/share/applications/SACTools.desktop
+    clink $out/share/eToken/shortcuts/SACMonitor.desktop $out/share/applications/SACMonitor.desktop
+    #if [ -d $out/share/gnome/autostart/ ] ; then
+      clink $out/share/eToken/shortcuts/SACMonitor.desktop $out/share/gnome/autostart/SACMonitor.desktop
+    #fi
+    clink $out/share/eToken/shortcuts/SafeNet.directory $out/share/desktop-directories/SafeNet.directory
+    clink $out/share/eToken/shortcuts/SafeNet-Authentication-Client.directory $out/share/desktop-directories/SafeNet-Authentication-Client.directory
+    #if [ -d $out/share/icons/gnome/32x32/apps ] ; then
+      clink $out/share/eToken/shortcuts/icons/SACTools.png $out/share/icons/gnome/32x32/apps/SACTools.png
+      clink $out/share/eToken/shortcuts/icons/SACMonitor.png $out/share/icons/gnome/32x32/apps/SACMonitor.png
+      touch $out/share/icons/gnome
+    #fi
+
+    # actual autostart
+    clink $out/share/eToken/shortcuts/SACMonitor.desktop $out/etc/xdg/autostart/SACMonitor.desktop
   '';
 }
